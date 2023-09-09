8f1ec613918aa212b2aeecf5718b5dc8057c084a
Frampt <112397341+IamFrampt@users.noreply.github.com>
2023-09-09 20:14:17 +0200

    Update SearchCrimeByCityFunction.cs

--- Snitches_function_API/SearchCrimeByCityFunction.cs
+++ Snitches_function_API/SearchCrimeByCityFunction.cs
@@ -26,7 +26,6 @@ namespace Snitches_function_API
         {
             _logger = log;
         }
-        //TEST24
 
         [FunctionName("SearchCrimeByCityName")]
         [OpenApiOperation(operationId: "Run")]
